8749ad57d11fa5ecd1874812a376237e0cd37727
IanZGray <ianzacharygray@gmail.com>
2022-10-26 18:10:11 -0400

    added cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     // 'http://localhost:3000', 
     'https://ian-graybankingapplication.s3.amazonaws.com',
-    'https://www.ian-graybankingapplication.s3.amazonaws.com'
+    'https://ian-graybankingapplication.s3.amazonaws.com/index.html'
     // do with www and without www for all sites on here
 ]
 
